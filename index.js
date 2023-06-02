1362d4346f54db97caff35f05b448f417d97ce09
MuazzezA <ayd.muazzez@gmail.com>
2023-06-02 14:32:32 +0300

    test mongodb connect

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT;
+
+async function connectMongoDB() {
+  try {
+    await mongoose.connect(MONGODB_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("MongoDB connection error: ", error);
+  }
+}
+connectMongoDB();
 
 const usersRouter = require("./routers/users-router.js");
-const server = express();
+
 const logger = require("./middlewares/logger.js");
 const errorHandling = require("./middlewares/error-handling.js");
 server.use(express.json()); // tüm istekrlerde body içerisindeki verilere ulaşabilmek için bunu parse ediyoruz
@@ -13,6 +28,6 @@ server.get("/", (req, res) => {
 server.use("/users", usersRouter);
 server.use(errorHandling); // tüm hatalar için error handling çalışacak - en sonda kullanılması daha iyi
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("http://localhost:3000 adresine gelen istekler dinleniyor...");
 });
